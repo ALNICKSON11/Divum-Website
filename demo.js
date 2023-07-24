86b8f48e8d6bc3a86407a2f25676ce7c1e999fa8
ALNICKSON11 <alnickson11@gmail.com>
2023-07-24 13:32:23 +0530

    updated it with one JS feature of image changing while clicking the list

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-const gallery = {1:"/image/Social Networks .jpg", 2:"/image/Supply Chain 002.jpg", 3:"/image/Media 003.jpg", 4:"/image/Fintech 004.jpg", 5:"/iamge/Health Tech.jpg", 6:"/image/E-commerce.jpg", 7:"/image/Travel Tech.jpg", 8:"/image/Edutech.jpg"}
+const gallery = {1:"/image/Social Networks .jpg", 2:"/image/Supply Chain 002.jpg", 3:"/image/Media 003.jpg", 4:"/image/Fintech 004.jpg", 5:"/image/Health Tech.jpg", 6:"/image/E-commerce.jpg", 7:"/image/Travel Tech.jpg", 8:"/image/Edutech.jpg"}
 
 function showImage(list) {
     document.getElementById("client-img").src = gallery[list.value]
